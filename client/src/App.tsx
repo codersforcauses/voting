8eff3902672633fbde2dc3f925557f65395136d5
Drew Alexander <drewalexander986@gmail.com>
2024-03-10 10:13:44 +0800

    add websocket to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
 function App() {
+  const { sendMessage, lastMessage, readyState } = useWebSocket("ws://localhost:8787/ws", {
+    shouldReconnect: (event) => true,
+  })
+
+  const handleClick = () => sendMessage(Math.random().toString())
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
   return (
     <>
-      woo
+      { connectionStatus }
+      <button onClick={handleClick}>Send Message</button>
+      { lastMessage?.data }
     </>
   )
 }
