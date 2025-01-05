4237c721a351de5a32eb51a1417283d45d586344
Drew Alexander <drewalexander986@gmail.com>
2025-01-05 22:43:19 +0800

    working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import { app } from "./controllers";
-import { stubContext } from "./middleware";
-import { Env, VotingObject } from "./models";
+import { Hono } from "hono";
+import { VotingObject } from "./voting";
+import { createMiddleware } from "hono/factory";
 
-export default {
-  async fetch(request, env, ctx) {
-      app.use(stubContext(env))
-      return app.fetch(request, env, ctx)
+export type Env = {
+  Bindings: {
+    VOTING_OBJECT: DurableObjectNamespace<VotingObject>;
+  },
+  Variables: {
+    stub: DurableObjectStub<VotingObject>
   }
-} satisfies ExportedHandler<Env>;
+}
+
+const app = new Hono<Env>()
+
+app.use(createMiddleware(async (c, next) => {
+    const id = c.env.VOTING_OBJECT.idFromName("main");
+    const stub = c.env.VOTING_OBJECT.get(id);
+    c.set('stub', stub)
+    await next()
+}))
+
+app.get('/', async (c) => {
+  const text = await c.var.stub.sayHello()
+  if (typeof text ===  'string') return c.text(text)
+    return c.text('No result found')
+})
+
+export default app
 
 export { VotingObject }
