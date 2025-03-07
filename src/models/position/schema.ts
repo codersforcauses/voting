4507607c92e3c38224a3c387edc54ebc89efe9ee
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2025-03-07 17:48:54 +0800

    force number storage

--- src/models/position/schema.ts
+++ src/models/position/schema.ts
@@ -2,20 +2,20 @@ import { DrizzleSqliteDODatabase } from "drizzle-orm/durable-sqlite";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const positionsTable = sqliteTable("positions_table", {
-    id: int().primaryKey({ autoIncrement: true }),
-    title: text().notNull(),
-    priority: int().notNull().unique(),
-})
+  id: int({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text().notNull(),
+  priority: int({ mode: "number" }).notNull().unique(),
+});
 
 export const seedPositions = (db: DrizzleSqliteDODatabase) => {
-    return db.insert(positionsTable).values([
-        { title: "President", priority: 1 },
-        { title: "Vice President", priority: 2 },
-        { title: "Secretary", priority: 3 },
-        { title: "Treasurer", priority: 4 },
-        { title: "Technical Lead", priority: 5 },
-        { title: "Marketing Officer", priority: 6 },
-        { title: "Ordinary Committee Member", priority: 7 },
-        { title: "Fresher Representative", priority: 8 }
-    ])
-}
+  return db.insert(positionsTable).values([
+    { title: "President", priority: 1 },
+    { title: "Vice President", priority: 2 },
+    { title: "Secretary", priority: 3 },
+    { title: "Treasurer", priority: 4 },
+    { title: "Technical Lead", priority: 5 },
+    { title: "Marketing Officer", priority: 6 },
+    { title: "Fresher Representative", priority: 7 },
+    { title: "Ordinary Committee Member", priority: 8 },
+  ]);
+};
