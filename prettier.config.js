aa897a5a9ff8fd2b5954997a82669c8e9b00ecb8
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2025-03-07 18:02:40 +0800

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  trailingComma: "all",
+  semi: false,
+  endOfLine: "lf",
+  printWidth: 120,
+  tabWidth: 2,
+};
+
+export default config;
