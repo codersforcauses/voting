339700f5ab006a0f198212b3d12f24716270098e
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2025-03-09 03:34:09 +0800

    query cfc user data

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,13 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", async (c) => {
-  return c.json("auth");
+  const id = "";
+  const fetchedData = await fetch(
+    `https://codersforcauses.org/api/trpc/user.get?batch=1&input={"0":{"json":"${id}"}}`
+  );
+  const data = await fetchedData.json();
+
+  return c.json(data[0].result.data.json);
 });
 
 export default app;
