1b866d5765f1adc05abbee0ffa5f377de288c31c
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2025-03-11 05:35:00 +0800

    use icon font

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@material-symbols/font-300/sharp.css";
 
 import type { Route } from "./+types/root";
 import "./app.css";
