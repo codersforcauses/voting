01ae3a08e0cbaeda6d75dd8f89f093bbb374e823
Lachlan Russell <lachlan@parkcedar.com>
2025-03-17 10:16:39 +1100

    fixed prettier formatting

--- src/lib/election-system/race.ts
+++ src/lib/election-system/race.ts
@@ -4,116 +4,116 @@ type Candidate = string;
 type Seat = string;
 
 export default class Race {
-	candidateVotes: Map<Candidate, Vote[]> = new Map();
-	seatVotes: Map<Seat, Vote> = new Map();
-	openings: number;
+  candidateVotes: Map<Candidate, Vote[]> = new Map();
+  seatVotes: Map<Seat, Vote> = new Map();
+  openings: number;
 
-	constructor(votes: Map<string, string[]>, openings: number = 1) {
-		this.openings = openings;
-		for (const [key, value] of votes) {
-			let vote = new Vote(key, value);
-			this.seatVotes.set(key, vote);
+  constructor(votes: Map<string, string[]>, openings: number = 1) {
+    this.openings = openings;
+    for (const [key, value] of votes) {
+      let vote = new Vote(key, value);
+      this.seatVotes.set(key, vote);
 
-			let tmp = this.candidateVotes.get(vote.first!);
+      let tmp = this.candidateVotes.get(vote.first!);
 
-			if (tmp) {
-				tmp.push(vote);
-			} else {
-				tmp = [vote];
-			}
+      if (tmp) {
+        tmp.push(vote);
+      } else {
+        tmp = [vote];
+      }
 
-			this.candidateVotes.set(vote.first!, tmp);
-		}
+      this.candidateVotes.set(vote.first!, tmp);
+    }
 
-		console.log(this.candidateVotes);
-	}
+    console.log(this.candidateVotes);
+  }
 
-	countVotes() {
-		let result: any[] = [];
+  countVotes() {
+    let result: any[] = [];
 
-		for (let [c, votes] of this.candidateVotes) {
-			let count = 0;
+    for (let [c, votes] of this.candidateVotes) {
+      let count = 0;
 
-			for (let v of votes) {
-				count += v.value;
-			}
-			result.push([c, count]);
-		}
-		return result;
-	}
+      for (let v of votes) {
+        count += v.value;
+      }
+      result.push([c, count]);
+    }
+    return result;
+  }
 
-	autocount() {
-		if (this.openings > 1) {
-			// Multi-candidate race - use hare-clark
-			return this.hareClarke();
-		}
-		// Instant Run-off
+  autocount() {
+    if (this.openings > 1) {
+      // Multi-candidate race - use hare-clark
+      return this.hareClarke();
+    }
+    // Instant Run-off
 
-		// First past the post
-	}
+    // First past the post
+  }
 
-	hareClarke() {
-		let elected = [];
-		const quota = Math.ceil(this.seatVotes.size / (this.openings + 1));
+  hareClarke() {
+    let elected = [];
+    const quota = Math.ceil(this.seatVotes.size / (this.openings + 1));
 
-		console.log(quota);
+    console.log(quota);
 
-		while (elected.length < this.openings) {
-			let buffer = [];
-			let count = this.countVotes();
+    while (elected.length < this.openings) {
+      let buffer = [];
+      let count = this.countVotes();
 
-			// Find candidates elected
-			// Need to change vote count to actually count the value
-			for (const c of count) {
-				if (c[1] >= quota) {
-					const transferValue = (c[1] - quota) / c[1];
-					buffer.push([c[0], transferValue]);
-					elected.push(c[0]);
-				}
-			}
+      // Find candidates elected
+      // Need to change vote count to actually count the value
+      for (const c of count) {
+        if (c[1] >= quota) {
+          const transferValue = (c[1] - quota) / c[1];
+          buffer.push([c[0], transferValue]);
+          elected.push(c[0]);
+        }
+      }
 
-			// Enough Candidates elected - can early exit here
-			if (elected.length >= this.openings) {
-				break;
-			}
+      // Enough Candidates elected - can early exit here
+      if (elected.length >= this.openings) {
+        break;
+      }
 
-			// Transfer votes
-			if (buffer.length > 0) {
-				// A candidate was elected so transfer their votes to the next candidate
-				for (let c of buffer) {
-					let candidate = c[0];
-					let transferValue = c[1];
-					if (transferValue > 0) {
-						for (let v of this.candidateVotes.get(candidate)!) {
-							v.value *= transferValue;
-							let next = v.next();
-							if (next) {
-								this.candidateVotes.get(next)?.push(v);
-							}
-						}
-					}
-					this.candidateVotes.delete(c[0]);
-				}
-			} else {
-				// TODO: Prematurely culling candidates. Need to re count after a distribution
-				// and work out how to deal with tie breakers.
-				// Transfer votes from lowest candidate
-				// Find candidate with min votes
-				const minCandidate = count.reduce((i, k) => (i[1] < k[1] ? i : k));
+      // Transfer votes
+      if (buffer.length > 0) {
+        // A candidate was elected so transfer their votes to the next candidate
+        for (let c of buffer) {
+          let candidate = c[0];
+          let transferValue = c[1];
+          if (transferValue > 0) {
+            for (let v of this.candidateVotes.get(candidate)!) {
+              v.value *= transferValue;
+              let next = v.next();
+              if (next) {
+                this.candidateVotes.get(next)?.push(v);
+              }
+            }
+          }
+          this.candidateVotes.delete(c[0]);
+        }
+      } else {
+        // TODO: Prematurely culling candidates. Need to re count after a distribution
+        // and work out how to deal with tie breakers.
+        // Transfer votes from lowest candidate
+        // Find candidate with min votes
+        const minCandidate = count.reduce((i, k) => (i[1] < k[1] ? i : k));
 
-				console.log(minCandidate);
-				// For each vote - pop candidate and append to second preference at current value
-				for (let v of this.candidateVotes.get(minCandidate[0])!.values()) {
-					let next = v.next();
-					if (next) {
-						this.candidateVotes.get(next)?.push(v);
-					}
-				}
+        console.log(minCandidate);
+        // For each vote - pop candidate and append to second preference at current value
+        for (let v of this.candidateVotes.get(minCandidate[0])!.values()) {
+          let next = v.next();
+          if (next) {
+            this.candidateVotes.get(next)?.push(v);
+          }
+        }
 
-				this.candidateVotes.delete(minCandidate[0]);
-				console.log(this.candidateVotes);
-			}
-		}
-		return elected;
-	}
+        this.candidateVotes.delete(minCandidate[0]);
+        console.log(this.candidateVotes);
+      }
+    }
+    return elected;
+  }
 }
--- src/lib/election-system/vote.ts
+++ src/lib/election-system/vote.ts
@@ -3,41 +3,41 @@
 
 */
 export default class Vote {
-	public seat: string;
-	public value: number = 1;
+  public seat: string;
+  public value: number = 1;
 
-	// The private _candidates is the reversed array
-	// use the candidates getter to access the in-order preferences
-	private _candidates: string[];
+  // The private _candidates is the reversed array
+  // use the candidates getter to access the in-order preferences
+  private _candidates: string[];
 
-	constructor(seat: string, votes: string[]) {
-		this.seat = seat;
-		this._candidates = votes.reverse();
-	}
+  constructor(seat: string, votes: string[]) {
+    this.seat = seat;
+    this._candidates = votes.reverse();
+  }
 
-	get candidates() {
-		return this._candidates.reverse();
-	}
+  get candidates() {
+    return this._candidates.reverse();
+  }
 
-	// Returns the effective first preference
-	get first() {
-		return this._candidates.at(-1);
-	}
+  // Returns the effective first preference
+  get first() {
+    return this._candidates.at(-1);
+  }
 
-	next() {
-		this._candidates.pop();
-		return this.first;
-	}
+  next() {
+    this._candidates.pop();
+    return this.first;
+  }
 
-	// Returns the next candidate that will get this vote out of a set of
-	// remaining candidates. Used to tie-break step 5 when two candidates have
-	// the same votes and we need to decide which candidate to remove from the
-	// count.
-	nextEffectiveVote(remainingCandidates: Set<string>) {
-		for (let c of this.candidates) {
-			if (c in remainingCandidates) {
-				return c;
-			}
-		}
-	}
+  // Returns the next candidate that will get this vote out of a set of
+  // remaining candidates. Used to tie-break step 5 when two candidates have
+  // the same votes and we need to decide which candidate to remove from the
+  // count.
+  nextEffectiveVote(remainingCandidates: Set<string>) {
+    for (let c of this.candidates) {
+      if (c in remainingCandidates) {
+        return c;
+      }
+    }
+  }
 }
