5c5a30a96ad68da12ba7815b41ffebc480a47b4f
Michael Nef <31727915+MouseAndKeyboard@users.noreply.github.com>
2025-03-21 01:46:40 +0800

    Update db.ts

--- src/middleware/db.ts
+++ src/middleware/db.ts
@@ -1,11 +1,14 @@
 import { factory } from "@/app";
-import { VotingObject } from "..";
+import { WackyVotingObject } from "..";
 import { env } from "hono/adapter";
 
+/**
+ * addStub sets up the comedic DO stub so that routes can talk to our WackyVotingObject.
+ */
 export const addStub = factory.createMiddleware(async (c, next) => {
-    const { VOTING_OBJECT } = env<{ VOTING_OBJECT: DurableObjectNamespace<VotingObject>}>(c);
+    const { VOTING_OBJECT } = env<{ VOTING_OBJECT: DurableObjectNamespace<WackyVotingObject>}>(c);
     const id = VOTING_OBJECT.idFromName("main");
-    const stub = VOTING_OBJECT.get(id) as DurableObjectStub<VotingObject>;
+    const stub = VOTING_OBJECT.get(id) as DurableObjectStub<WackyVotingObject>;
     c.set('STUB', stub)
     await next()
 })
