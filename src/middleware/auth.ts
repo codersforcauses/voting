9b463ace6b252e8ca4ec6c766806a887151eab00
Michael Nef <31727915+MouseAndKeyboard@users.noreply.github.com>
2025-03-21 01:46:56 +0800

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,28 +3,34 @@ import { env } from "hono/adapter";
 import { HTTPException } from "hono/http-exception";
 import { verify } from "hono/jwt";
 
-export const authenticate = factory.createMiddleware(async (c, next) => {
+/**
+ * comedic authentication middleware
+ */
+export const comedicAuthenticate = factory.createMiddleware(async (c, next) => {
   const { AUTH_SECRET_KEY } = env<{ AUTH_SECRET_KEY: string }>(c);
   const token = c.req.header("Authorization")?.split("Bearer ")[1];
 
   if (!token) {
-    throw new HTTPException(401, { message: "No token provided" });
+    throw new HTTPException(401, { message: "No comedic token provided" });
   }
   try {
     const verified = await verify(token, AUTH_SECRET_KEY);
     c.set("ID", verified.sub);
     c.set("ROLE", verified.role);
   } catch (err) {
-    throw new HTTPException(401, { message: "Invalid token" });
+    throw new HTTPException(401, { message: "Invalid comedic token" });
   }
   await next();
 });
 
-export const requireAdmin = factory.createMiddleware(async (c, next) => {
+/**
+ * comedic admin requirement
+ */
+export const comedicRequireAdmin = factory.createMiddleware(async (c, next) => {
   const role = c.get("ROLE");
 
   if (role !== "admin") {
-    throw new HTTPException(403, { message: "Unauthorized" });
+    throw new HTTPException(403, { message: "Unauthorized comedic action" });
   }
   await next();
 });
