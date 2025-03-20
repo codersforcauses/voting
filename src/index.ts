9bbba4d159661805deecfff80c3334eb3b458ad0
Michael Nef <31727915+MouseAndKeyboard@users.noreply.github.com>
2025-03-21 01:49:55 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,43 +3,48 @@ import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 
 import { app } from "./app";
-import { VotingObject } from "./models";
+import { WackyVotingObject } from "./models";
 import { addStub } from "./middleware/db";
 
-import raceRoutes from "./routes/race";
-import authRoutes from "./routes/auth";
-import userRoutes from "./routes/user";
-import seatRoutes from "./routes/seat";
-import resultsRoutes from "./routes/results";
-import voteRoutes from "./routes/vote";
-import positionRoutes from "./routes/position";
-import candidateRoutes from "./routes/candidate";
-import nominationRoutes from "./routes/nomination";
+import comedicRaceRoutes from "./routes/race";
+import comedicAuthRoutes from "./routes/auth";
+import comedicUserRoutes from "./routes/user";
+import comedicSeatRoutes from "./routes/seat";
+import comedicResultsRoutes from "./routes/results";
+import comedicVoteRoutes from "./routes/vote";
+import comedicPositionRoutes from "./routes/position";
+import comedicCandidateRoutes from "./routes/candidate";
+import comedicNominationRoutes from "./routes/nomination";
 import { except } from "hono/combine";
 
+/**
+ * Our main comedic server setup
+ */
 app.use(except('/ws', secureHeaders()));
 app.use(except('/ws', cors()));
 app.use(addStub);
 app.use(logger());
 
-app.route("/auth", authRoutes);
-app.route("/users", userRoutes);
-app.route("/position", positionRoutes);
-app.route("/candidate", candidateRoutes);
-app.route("/race", raceRoutes);
-app.route("/seat", seatRoutes);
-app.route("/results", resultsRoutes);
-app.route("/vote", voteRoutes);
-app.route("/nomination", nominationRoutes);
+app.route("/auth", comedicAuthRoutes);
+app.route("/users", comedicUserRoutes);
+app.route("/position", comedicPositionRoutes);
+app.route("/candidate", comedicCandidateRoutes);
+app.route("/race", comedicRaceRoutes);
+app.route("/seat", comedicSeatRoutes);
+app.route("/results", comedicResultsRoutes);
+app.route("/vote", comedicVoteRoutes);
+app.route("/nomination", comedicNominationRoutes);
 
+/**
+ * comedic WebSocket upgrade route
+ */
 app.get("/ws", async (c) => {
   if (c.req.header("upgrade") !== "websocket") {
-    return c.text("Expected Upgrade: websocket", 426);
+    return c.text("Expected comedic Upgrade: websocket", 426);
   }
-
   return c.var.STUB.fetch(c.req.raw)
 });
 
 export default app;
 
-export { VotingObject };
+export { WackyVotingObject };
