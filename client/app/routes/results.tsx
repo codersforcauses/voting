cafb8e5a1a568820f8adbd018fa234fe14b9115d
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2025-03-21 15:42:23 +0800

    change from flex to grid

--- client/app/routes/results.tsx
+++ client/app/routes/results.tsx
@@ -25,7 +25,7 @@ export default function Results() {
   const [elected, refetchElected] = useResults(currentRace?.race.id);
 
   const { data: count } = useQuery({
-    enabled: currentRace?.race.status === 'open',
+    enabled: currentRace?.race.status === "open",
     queryKey: ["count", currentRace?.race.id],
     queryFn: async () => {
       const res = await fetch(
@@ -48,47 +48,58 @@ export default function Results() {
     if (wsData?.status === "finished") refetchElected();
   }, [wsData?.status]);
 
-
   return (
     <main className="flex flex-col gap-3 justify-center items-center h-screen">
       {currentRace?.race.status === "closed" && (
         <>
           <div className="text-2xl">Nominees</div>
-          <div className="text-5xl  mb-10">{currentRace?.positions.title}</div>
-          <div className="flex gap-4">
-          {candidates.map((candidate) => (
-            <div key={candidate.id} className="flex justify-center items-center w-50 h-50 text-background bg-foreground text-4xl font-mono">
-              {candidate.name}
-            </div>
-          ))}
-        </div>
-      </>
+          <div className="text-5xl mb-10">{currentRace?.positions.title}</div>
+          <div className="grid w-full place-items-center grid-cols-[repeat(auto-fit,minmax(--spacing(50),1fr))] gap-4">
+            {candidates.map((candidate) => (
+              <div
+                key={candidate.id}
+                className="flex justify-center items-center size-50 text-background bg-foreground text-4xl font-mono"
+              >
+                {candidate.name}
+              </div>
+            ))}
+          </div>
+        </>
       )}
       {currentRace?.race.status === "open" && (
         <>
           <div className="text-5xl  mb-10">{currentRace?.positions.title}</div>
-          <div className="text-6xl text-foreground font-mono">Commence Voting</div>
+          <div className="text-6xl text-foreground font-mono">
+            Commence Voting
+          </div>
           {count && (
-            <div className="text-2xl">{count.votes.toString()}/{count.users}</div>
+            <div className="text-2xl">
+              {count.votes.toString()}/{count.users}
+            </div>
           )}
         </>
       )}
 
       {currentRace?.race.status === "finished" && (
         <>
-          <div className="text-2xl">Winner{elected && elected.length > 1 && 's'}</div>
+          <div className="text-2xl">
+            Winner{elected && elected.length > 1 && "s"}
+          </div>
           <div className="text-5xl  mb-10">{currentRace?.positions.title}</div>
-            <div className="flex gap-4">
-            {elected && elected.map((candidate) => (
-              <div key={candidate.candidates.id} className="flex text-center justify-center items-center w-50 h-50 text-background bg-foreground text-4xl font-mono">
-                {candidate.candidates.name}
-              </div>
-            )
-          )}
-        </div>
-      </>
-      )}  
-      
+          <div className="flex gap-4">
+            {elected &&
+              elected.map((candidate) => (
+                <div
+                  key={candidate.candidates.id}
+                  className="flex text-center justify-center items-center w-50 h-50 text-background bg-foreground text-4xl font-mono"
+                >
+                  {candidate.candidates.name}
+                </div>
+              ))}
+          </div>
+        </>
+      )}
+
       {/* <ResultGraph></ResultGraph> */}
     </main>
   );
