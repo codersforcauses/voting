1834eeaeaa265cb501be2790613914e3144a02a3
Lachlan Russell <lachlan@parkcedar.com>
2025-03-24 15:20:45 +1100

    add methods without writing them all out

---
+++ src/models/db/index.ts
@@ -0,0 +1,21 @@
+import * as candidate from "./candidate";
+import * as elected from "./elected";
+import * as nomination from "./nomination";
+import * as position from "./position";
+import * as race from "./race";
+import * as seat from "./seat";
+import * as user from "./user";
+import * as pref from "./vote-preference";
+import * as vote from "./vote";
+
+export {
+	candidate,
+	elected,
+	nomination,
+	position,
+	race,
+	seat,
+	user,
+	pref,
+	vote
+}
